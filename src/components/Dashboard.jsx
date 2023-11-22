e0b52832d2124d9c64087f832a5eab7cfd79e63b
Duke Kongo <dukekongo16@gmail.com>
2023-11-22 07:54:44 +0000

    Eight Commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import FormCss from '../Form.module.css'
 
 
 function Dashboard(props) {
+    const [, forceUpdate] = useState();
 
     const highestScore = localStorage.getItem('highestScore') || 0;
 
+    const clearHighestScore = (e) => {
+
+        if (highestScore > 0) {
+            localStorage.removeItem("highestScore", "0");
+
+            forceUpdate(prevState => !prevState);
+        }
+    }
     return (
         <div>
             <h1 className={FormCss.allset}>Highest Score: {highestScore}</h1>
+
+            <div className={FormCss.form}>
+                <button
+                    className={FormCss.form__button}
+                    onClick={clearHighestScore}>Reset High Score</button>
+            </div>
         </div>
     );
 }
